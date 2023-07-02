937791d1020b03764ea7dc59245dda1fa568c16e
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-02 15:45:20 +0900

    feat: 돈이라는 값 객체 생성 및 편의 메서드 생성

---
+++ hexagonal-architecture/src/main/java/com/geunskoo/hexagonalarchitecture/domain/Money.java
@@ -0,0 +1,47 @@
+package com.geunskoo.hexagonalarchitecture.domain;
+
+import java.math.BigInteger;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+public record Money(@NonNull BigInteger amount) {
+
+    public static Money ZERO = Money.of(0L);
+
+    public boolean isPositiveOrZero() {
+        return this.amount.compareTo(BigInteger.ZERO) >= 0;
+    }
+
+    public boolean isNegative() {
+        return this.amount.compareTo(BigInteger.ZERO) < 0;
+    }
+
+    public boolean isGreaterThanOrEqualTo(Money money) {
+        return this.amount.compareTo(money.amount) >= 1;
+    }
+
+    public static Money of(long value) {
+        return new Money(BigInteger.valueOf(value));
+    }
+
+    public static Money add(Money money1, Money money2) {
+        return new Money(money1.amount.add(money2.amount));
+    }
+
+    public static Money substract(Money money1, Money money2) {
+        return new Money(money1.amount.subtract(money2.amount));
+    }
+
+    public Money minus(Money money) {
+        return new Money(this.amount.subtract(money.amount));
+    }
+
+    public Money plus(Money money) {
+        return new Money(this.amount.add(money.amount));
+    }
+
+    public Money negate() {
+        return new Money(this.amount.negate());
+    }
+}
