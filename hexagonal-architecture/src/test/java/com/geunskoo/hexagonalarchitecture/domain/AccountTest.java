adc528b5d2f38fda1f5eb39e418def653b51b6e1
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-04 15:43:44 +0900

    refactor: @Nest, @DisplayName을 활용하여 더 명시적인 테스트 코드로 변경

--- hexagonal-architecture/src/test/java/com/geunskoo/hexagonalarchitecture/domain/AccountTest.java
+++ hexagonal-architecture/src/test/java/com/geunskoo/hexagonalarchitecture/domain/AccountTest.java
@@ -6,106 +6,125 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.geunskoo.hexagonalarchitecture.domain.Account.AccountId;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 
 class AccountTest {
 
-    @Test
-    @DisplayName("계좌 인출 성공 테스트.")
-    void withdrawSucceeds() {
-        //given
-        AccountId accountId = new AccountId(3L);
-        Account account = defaultAccount()
-            .withAccountId(accountId)
-            .withBaselineBalance(Money.of(500L))
-            .withActivityWindow(new ActivityWindow(
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(999L)).build(),
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(1L)).build()
-            )).build();
+    @Nested
+    @DisplayName("계좌 인출 테스트")
+    class withdraw {
 
-        //when
-        boolean success = account.withdraw(Money.of(500), new AccountId(4L));
+        @Test
+        @DisplayName("인출 성공")
+        void withdrawSucceeds() {
+            //given
+            AccountId accountId = new AccountId(3L);
+            Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(new ActivityWindow(
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(999L)).build(),
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(1L)).build()
+                )).build();
 
-        //then
-        assertThat(success).isTrue();
-        assertThat(account.getActivityWindow().getActivityList()).hasSize(3);
-        assertThat(account.calculateBalance()).isEqualTo(Money.of(1000L));
+            //when
+            boolean success = account.withdraw(Money.of(500), new AccountId(4L));
+
+            //then
+            assertThat(success).isTrue();
+            assertThat(account.getActivityWindow().getActivityList()).hasSize(3);
+            assertThat(account.calculateBalance()).isEqualTo(Money.of(1000L));
+        }
+
+        @Test
+        @DisplayName("인출 실패")
+        void withdrawFailure() {
+            //given
+            AccountId accountId = new AccountId(3L);
+            Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(new ActivityWindow(
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(999L)).build(),
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(1L)).build()
+                )).build();
+
+            //when
+            boolean failure = account.withdraw(Money.of(1501L), new AccountId(4L));
+
+            //then
+            assertThat(failure).isFalse();
+            assertThat(account.getActivityWindow().getActivityList()).hasSize(2);
+            assertThat(account.calculateBalance()).isEqualTo(Money.of(1500L));
+        }
     }
 
-    @Test
-    @DisplayName("계좌 인출 실패 테스트.")
-    void withdrawFailure() {
-        //given
-        AccountId accountId = new AccountId(3L);
-        Account account = defaultAccount()
-            .withAccountId(accountId)
-            .withBaselineBalance(Money.of(500L))
-            .withActivityWindow(new ActivityWindow(
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(999L)).build(),
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(1L)).build()
-            )).build();
+    @Nested
+    @DisplayName("계좌 예금 테스트")
+    class deposit {
 
-        //when
-        boolean failure = account.withdraw(Money.of(1501L), new AccountId(4L));
+        @Test
+        @DisplayName("예금 성공")
+        void depositSuccess() {
+            //given
+            AccountId accountId = new AccountId(3L);
+            Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(new ActivityWindow(
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(999L)).build(),
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(1L)).build()
+                )).build();
+            //when
+            boolean success = account.deposit(Money.of(500L), new AccountId(4L));
 
-        //then
-        assertThat(failure).isFalse();
-        assertThat(account.getActivityWindow().getActivityList()).hasSize(2);
-        assertThat(account.calculateBalance()).isEqualTo(Money.of(1500L));
+            //then
+            assertThat(success).isTrue();
+            assertThat(account.getActivityWindow().getActivityList()).hasSize(3);
+            assertThat(account.calculateBalance()).isEqualTo(Money.of(2000L));
+        }
     }
 
-    @Test
-    @DisplayName("계좌 예금 성공 테스트.")
-    void depositSuccess() {
-        //given
-        AccountId accountId = new AccountId(3L);
-        Account account = defaultAccount()
-            .withAccountId(accountId)
-            .withBaselineBalance(Money.of(500L))
-            .withActivityWindow(new ActivityWindow(
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(999L)).build(),
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(1L)).build()
-            )).build();
-        //when
-        boolean success = account.deposit(Money.of(500L), new AccountId(4L));
+    @Nested
+    @DisplayName("계좌 잔액 조회 테스트")
+    class calculateBalanceTest {
 
-        //then
-        assertThat(success).isTrue();
-        assertThat(account.getActivityWindow().getActivityList()).hasSize(3);
-        assertThat(account.calculateBalance()).isEqualTo(Money.of(2000L));
+        @Test
+        @DisplayName("잔액 조회 성공")
+        void calculateBalance() {
+            //given
+            AccountId accountId = new AccountId(3L);
+            Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(new ActivityWindow(
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(999L)).build(),
+                    defaultActivity()
+                        .withTargetAccount(accountId)
+                        .withMoney(Money.of(1L)).build()
+                )).build();
+            //when
+            Money balance = account.calculateBalance();
+            //then
+            assertThat(balance).isEqualTo(Money.of(1500L));
+        }
     }
 
-    @Test
-    @DisplayName("계좌 현재 잔액 불러오기 테스트.")
-    void calculateBalance() {
-        //given
-        AccountId accountId = new AccountId(3L);
-        Account account = defaultAccount()
-            .withAccountId(accountId)
-            .withBaselineBalance(Money.of(500L))
-            .withActivityWindow(new ActivityWindow(
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(999L)).build(),
-                defaultActivity()
-                    .withTargetAccount(accountId)
-                    .withMoney(Money.of(1L)).build()
-            )).build();
-        //when
-        Money balance = account.calculateBalance();
-        //then
-        assertThat(balance).isEqualTo(Money.of(1500L));
-    }
+
 }
