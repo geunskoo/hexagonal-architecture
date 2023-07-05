61da643b6ae4f50bedf5b802790a3e12ad7386b4
geunskoo <oryukdo3@pusan.ac.kr>
2023-07-06 00:02:35 +0900

    feat: 송금 관련 규칙을 담당하는 객체 생성. 이체 최대 금액 규정.

---
+++ hexagonal-architecture/src/main/java/com/geunskoo/hexagonalarchitecture/application/service/MoneyTransferProperties.java
@@ -0,0 +1,14 @@
+package com.geunskoo.hexagonalarchitecture.application.service;
+
+import com.geunskoo.hexagonalarchitecture.domain.Money;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoneyTransferProperties {
+
+    private Money maximumTransferThreshold = Money.of(1_000_000L);
+}
